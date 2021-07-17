2f160ca397c600980cdf6581305c6269af734f6e
Christopher Warrington <code@cw.codes>
2021-07-17 17:39:10 -0400

    Print local socket in client

--- client/src/main.rs
+++ client/src/main.rs
@@ -9,8 +9,9 @@ pub async fn main() -> Result<()> {
     let remote_addr = std::env::args().nth(1).expect("no remote endpoint given");
 
     let tcp_stream = TcpStream::connect(&remote_addr).await?;
+    let local_addr = tcp_stream.local_addr()?;
     let peer_addr = tcp_stream.peer_addr()?;
-    println!("Connected to {} ({})", peer_addr, &remote_addr);
+    println!("Connected to {} ({}) from {}", peer_addr, &remote_addr, local_addr);
     
     let (rd, wr) = io::split(tcp_stream);
         
