ed4024ce9bf51bfb3c25423a236b0733e94c46e1
Christopher Warrington <code@cw.codes>
2021-07-17 17:28:02 -0400

    Add client

---
+++ client/src/main.rs
@@ -0,0 +1,57 @@
+
+use rand::prelude::*;
+use std::io::Result;
+use std::net::SocketAddr;
+use tokio::net::TcpStream;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt, ReadHalf, WriteHalf};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let remote_addr = std::env::args().nth(1).expect("no remote endpoint given");
+
+    let tcp_stream = TcpStream::connect(&remote_addr).await?;
+    let peer_addr = tcp_stream.peer_addr()?;
+    println!("Connected to {} ({})", peer_addr, &remote_addr);
+    
+    let (rd, wr) = io::split(tcp_stream);
+        
+    let read_handler = tokio::spawn(async move {
+        match handle_read(rd, &peer_addr).await {
+            Ok(()) => println!("Remote {} read hung up gracefully", peer_addr),
+            Err(e) => println!("Remote {} read hung up with error {}", peer_addr, e),
+        };
+    });
+
+    let write_handler = tokio::spawn(async move {
+        match handle_write(wr).await {
+            Ok(()) => println!("Remote {} write hung up gracefully", peer_addr),
+            Err(e) => println!("Remote {} write hung up with error {}", peer_addr, e),
+        }
+    });
+
+    tokio::try_join!(read_handler, write_handler)?;
+    Ok(())
+}
+
+async fn handle_read(mut read_socket: ReadHalf<TcpStream>, remote_addr: &SocketAddr) -> Result<()> {
+    let mut buf = vec![0; 4096];
+
+    loop {
+        let bytes_read = read_socket.read(&mut buf).await?;
+
+        if bytes_read == 0 {
+            return Ok(());
+        }
+
+        println!("Read {} bytes from {}", bytes_read, remote_addr);
+    }
+}
+
+async fn handle_write(mut write_socket: WriteHalf<TcpStream>) -> Result<()> {
+    let mut buf: Vec<u8> = vec![0; 4096];
+
+    loop {
+        thread_rng().fill(&mut buf[..]);
+        write_socket.write_all(&buf).await?;
+    }
+}
